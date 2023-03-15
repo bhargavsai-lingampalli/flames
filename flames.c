0f34c58ace415a177f497641b2c4e2e93af59344
Bhargav Sai <114660786+BhargavSai-Lingampalli@users.noreply.github.com>
2023-03-15 19:09:08 +0530

    Update flames.c
    updated the code

--- flames.c
+++ flames.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
-#include<string.h>
-void uniform(char a[],int size);
+
+int len(char*);
+void uniform(char *);
 int main()
 {
     int i,j,flames_check[6]= {1,1,1,1,1,1};
-    int limit=5,del=1,diff,result;
+    int striked=0,del=1,diff=0;
     char name1[100],name2[100];
-    char flames[6][15]= {"friends","lovers","attraction"
-                         ,"marriage","enemies","siblings"
-                        };
+    char flames[6][15]= {"friends","lovers","attraction","marriage","enemies","siblings"};
+
     printf("Enter Name1: ");
-    gets(name1);
+    scanf("%[^\n]",name1);
     printf("Enter Name2: ");
-    gets(name2);
-    uniform(name1,strlen(name1));
-    uniform(name2,strlen(name2));
-    diff=strlen(name1)+strlen(name2);
-    for(i=0; i<strlen(name1); i++)
+    scanf("%*c%[^\n]",name2);
+
+    uniform(name1);
+    uniform(name2);
+
+    diff=len(name1)+len(name2);
+    for(i=0; name1[i]!='\0'; i++)
     {
-        for(j=0; j<strlen(name2); j++)
+        for(j=0; name2[j]!='\0'; j++)
         {
             if(name1[i]==name2[j])
-            {   diff=diff-2;
+            {   diff-=2;
                 name2[j]='?'; //striking
                 break;
             }
         }
     }
 
-    for(i=0,del=1; limit>0 && diff!=0; i++)
+    for(i=0,del=1; striked<5 && diff!=0; i++)
     {
         if(flames_check[i%6]!=0 && del==diff)
         {
             flames_check[i%6]=0;
             del=1;
-            limit--;
+            striked++;
         }
         if(flames_check[i%6]!=0 && del<diff)
             del+=1;
@@ -43,25 +45,30 @@ int main()
     for(i=0; i<6; i++)
     {
         if(flames_check[i]!=0)
-            result=i;
+            printf("%s\n",flames[i]);
     }
-    printf("%s\n",flames[result]);
 
     return 0;
 }
 
-void uniform(char a[],int size)
+void uniform(char *name)
 {
     int i,j;
-    for(i=0; i<size; i++)
+    for(i=0; name[i]!='\0'; i++)
     {
-        if(a[i]>96)
-            a[i]=a[i]-32;
-        if(a[i]==' ')
+        if(name[i]>96)
+            name[i]=name[i]-32;
+        if(name[i]==' ')
         {
-            for(j=i; j<size; j++)
-                a[j]=a[j+1];
+            for(j=i; name[j]!='\0'; j++)
+                name[j]=name[j+1];
+            i--;
         }
     }
-
+}
+int len(char *name)
+{
+    int c;
+    for(c=0; name[c]!='\0'; c++);
+    return c;
 }
